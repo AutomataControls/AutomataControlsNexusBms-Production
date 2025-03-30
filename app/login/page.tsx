91782883b56ad6c62093300652cc7e99b0bbe344
andrew jewell <automatacontrols@gmail.com>
2025-03-30 12:21:19 -0400

    Update file page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -328,7 +328,7 @@ export default function LoginPage() {
                     </DialogContent>
                   </Dialog>
                 </div>
-                {/* Google sign-in section - temporarily disabled
+                {/* Google sign-in section - enabled */}
                 <div className="w-full space-y-4">
                   <div className="relative">
                     <div className="absolute inset-0 flex items-center">
@@ -366,8 +366,7 @@ export default function LoginPage() {
                       Continue with Google
                     </Button>
                   </div>
-                </div>
-                */}
+                </div>                
               </CardFooter>
             </form>
           </Tabs>
